08c741251aa831059dda86c3b969692a961f6650
Adriano Rodrigues <adrianojrodrigues12@gmail.com>
2022-05-14 17:44:58 -0300

    formulaflix front concluido

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        768:{
+            items:4
+        },
+        1024:{
+            items:4
+        },
+        1900: {
+            items:6
+        }
+    }
+})
